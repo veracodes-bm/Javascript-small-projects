7b041893eea3219c25e5a97181f3a2b909694cff
Vera <110102242+veracodes-bm@users.noreply.github.com>
2022-11-10 09:10:26 +0100

    Add files via upload

---
+++ Scroll-Gallary/script.js
@@ -0,0 +1,22 @@
+
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach( (panel)=>{
+
+    panel.addEventListener('click', ()=>{
+
+        removeActiveclasses()
+
+        panel.classList.add('active')
+       
+    })
+
+})
+
+function removeActiveclasses () {
+    panels.forEach( (panel)=>{
+        panel.classList.remove('active')
+
+    })
+}
