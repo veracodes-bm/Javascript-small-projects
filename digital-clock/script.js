8b2a64e30e3fd1ed67fed6810032079473cf37e6
Vera <110102242+veracodes-bm@users.noreply.github.com>
2022-11-10 09:04:47 +0100

    Add files via upload

---
+++ digital-clock/script.js
@@ -0,0 +1,32 @@
+
+
+const hourEl = document.getElementById('hour')
+const minuteEl = document.getElementById('minuets')
+const secondEl = document.getElementById('seconds')
+const ampmEl = document.getElementById('ampm')
+
+
+
+
+function updateClock() {
+     let h = new Date().getHours()
+     let m = new Date().getMinutes()
+     let s = new Date().getSeconds()
+
+     if (h>12)  {
+        h = h - 12
+        ampm = 'pm'
+
+     }
+
+     h = h < 10 ? '0' + h : h;
+     m = m < 10 ? '0' + m : m;
+     s = s < 10 ? '0' + s : s;
+    
+     hourEl.innerText = h;
+     minuteEl.innerText = m;
+     secondEl.innerText= s;
+
+     setTimeout(updateClock, 1000)
+    }
+    updateClock()
